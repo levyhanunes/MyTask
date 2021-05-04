28b7bbfc8d3f4f2c02d3dddb674f1d51b1f748a2
levyhanunes <levyhanunes@gmail.com>
2021-05-03 22:47:05 -0400

    Populando as Tasks atraves do numbersOfRowsInSection e cellForRowAt

---
+++ MyTask/Controllers/Cells/TaskTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TaskTableViewCell.swift
+//  MyTask
+//
+//  Created by user195088 on 5/3/21.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var categotyNameLabel: UILabel!
+    @IBOutlet weak var taskDescriptionLabel: UILabel!
+    @IBOutlet weak var categoryView: UIView!
+    
+}
--- MyTask/Controllers/TasksTableViewController.swift
+++ MyTask/Controllers/TasksTableViewController.swift
@@ -9,18 +9,43 @@ import UIKit
 
 
 let category : Category = Category(name: "Lista de compras", color: .green)
-let task : [Task] =  [
+let tasks : [Task] =  [
     
-    Task(name: "Entregar documentos de estagio na BRQ :)", date: Date(), category: category)
-    Task(name: "Essa e a minha lista de compras", date: Date(), category: category)
+    Task(name: "Entregar documentos de estagio na BRQ :)", date: Date(), category: category),
+    Task(name: "Essa e a minha lista de compras", date: Date(), category: category),
     Task(name: "Criar uma apresentacao para amanha", date: Date(), category: category)
 ]
 
 class TasksTableViewController: UITableViewController {
 
+    
+    private var dateFormatter : DateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskTableViewCell
+        let task = tasks[indexPath.row]
+        
+        dateFormatter.dateFormat = "HH:mm"
+        cell.hourLabel.text = dateFormatter.string(from: task.date)
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        cell.dateLabel.text = dateFormatter.string(from: task.date)
+        
+        
+        cell.categotyNameLabel.text = task.category.name
+        
+        cell.categoryView.backgroundColor = task.category.color
+        
+        cell.taskDescriptionLabel.text = task.name
+        
+        return cell
+    }
 }
